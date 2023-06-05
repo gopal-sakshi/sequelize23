3c4a5d70352e10b34e58fccbd6039bac590b6de4
gopal-sakshi <gopal612@gmail.com>
2023-06-05 22:18:36 +0530

    1st commit

---
+++ connection.js
@@ -0,0 +1,20 @@
+const {Sequelize} = require("sequelize");
+// const sequelize = new Sequelize("sqlite::memory:");
+const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: "C:\\Users\\GopAL\\Desktop\\backEnd\\sequelize23\\db23\\sqllite12.db"
+});
+const BlogPosts = require('./models/posts');
+
+return sequelize.authenticate()
+    .then(result => {
+        console.log(`SQLite successfully connected!`);
+        return BlogPosts.sync();
+    })
+    .then(result => {
+        console.log(`Blog Posts table created`);
+        return result;
+    })
+    .catch(error => {
+        console.error('Unable to connect to SQLite database:', error);
+    })
